28fbe12027be656e92c2f25c6f26699628a8e9dd
Connor Slattery <cslat@illinois.edu>
2026-09-20 12:57:02 -0500

    Add dataset-manual route

--- data_quality/urls.py
+++ data_quality/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("datasets/<int:pk>/", views.DatasetDetailView.as_view(), name="dataset-detail"),
 
     # --- FBVs (teammate section - add here once written) ---
-    # path("datasets/manual/", views.dataset_manual, name="dataset-manual"),
+    path("datasets/manual/", views.dataset_manual, name="dataset-manual"),
     # path("datasets/render/", views.dataset_render, name="dataset-render"),
 ]
